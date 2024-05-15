a71c258ff636d92a55feebbed765242cc5195e26
Butouski Mikita <nindse@gmail.com>
2024-05-15 18:12:31 +0200

    Main app is ready. Page index and about are ready.

--- Online_shop/urls.py
+++ Online_shop/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for Online_shop1 project.
+URL configuration for Online_shop project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
@@ -14,9 +14,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
+
 from django.urls import path
+from main import views
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', views.index, name='index'),
+    path('about/', views.about, name='about'),
 ]
--- Online_shop/wsgi.py
+++ Online_shop/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for Online_shop1 project.
+WSGI config for Online_shop project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Online_shop1.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Online_shop.settings')
 
 application = get_wsgi_application()
